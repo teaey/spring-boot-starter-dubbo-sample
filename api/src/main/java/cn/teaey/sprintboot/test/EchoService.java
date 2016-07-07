11b4ed70dbf76a404f8a9806b5b5935ee1dc79eb
teaey <masfay@me.com>
2016-07-07 17:28:09 +0800

    Init

---
+++ api/src/main/java/cn/teaey/sprintboot/test/EchoService.java
@@ -0,0 +1,9 @@
+package cn.teaey.sprintboot.test;
+
+/**
+ * @author xiaofei.wxf(teaey)
+ * @since 0.0.0
+ */
+public interface EchoService {
+    String echo(String str);
+}
---
+++ server/src/main/java/cn/teaey/sprintboot/test/Server.java
@@ -0,0 +1,16 @@
+package cn.teaey.sprintboot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiaofei.wxf(teaey)
+ * @since 0.0.0
+ */
+@SpringBootApplication
+public class Server {
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(Server.class, args);
+
+    }
+}
